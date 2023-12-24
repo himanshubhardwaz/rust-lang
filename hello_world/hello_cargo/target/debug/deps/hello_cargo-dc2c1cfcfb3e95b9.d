679d3b5c44f4156e90a52d02a31648195451e521
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2023-12-24 19:15:22 +0530

    hello world

---
+++ hello_world/hello_cargo/target/debug/deps/hello_cargo-dc2c1cfcfb3e95b9.d
@@ -0,0 +1,5 @@
+/Users/himanshubhardwaz/personal/rust-lang/hello_world/hello_cargo/target/debug/deps/hello_cargo-dc2c1cfcfb3e95b9: src/main.rs
+
+/Users/himanshubhardwaz/personal/rust-lang/hello_world/hello_cargo/target/debug/deps/hello_cargo-dc2c1cfcfb3e95b9.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/himanshubhardwaz/personal/rust-lang/hello_world/hello_cargo/target/debug/hello_cargo: /Users/himanshubhardwaz/personal/rust-lang/hello_world/hello_cargo/src/main.rs
